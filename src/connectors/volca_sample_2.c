0eca8d44c6ab133c6fe43da20e211f15b3ca7902
dagargo <dagargo@gmail.com>
2026-07-29 20:09:55 +0200

    volca-sample-2: Rename variable
    Revert wrong renaming in a89133610021cd7677b2cae5ffa54b9833e5198e.

--- src/connectors/volca_sample_2.c
+++ src/connectors/volca_sample_2.c
@@ -353,7 +353,7 @@ volca_sample_2_sample_upload_params (struct backend *backend,
 				     struct task_control *control)
 {
   gint err;
-  guint id, gsizeruncated;
+  guint id, size_truncated;
   guint8 *buff;
   guint buff_size;
   guint8 header_dump[39];
@@ -380,8 +380,8 @@ volca_sample_2_sample_upload_params (struct backend *backend,
   memcpy (header.name, name, MIN (strlen (name),
 				  VOLCA_SAMPLE_2_SAMPLE_NAME_LEN));
   // TODO: If sample is truncated, the return value should indicate this.
-  gsizeruncated = volca_sample_2_sample_get_max_size_in_msg (size);
-  header.frames = gsizeruncated / sizeof (gint16);
+  size_truncated = volca_sample_2_sample_get_max_size_in_msg (size);
+  header.frames = size_truncated / sizeof (gint16);
   header.level = level;
   header.speed = speed;
 
@@ -409,10 +409,10 @@ volca_sample_2_sample_upload_params (struct backend *backend,
 
   g_usleep (VOLCA_SAMPLE_2_REST_TIME_US);
 
-  buff_size = 2 + common_8bit_msg_to_midi_msg_size (gsizeruncated);
+  buff_size = 2 + common_8bit_msg_to_midi_msg_size (size_truncated);
   buff = g_malloc (buff_size);
   volca_sample_2_set_sample_id (buff, id);
-  common_8bit_msg_to_midi_msg (data, &buff[2], gsizeruncated);
+  common_8bit_msg_to_midi_msg (data, &buff[2], size_truncated);
 
   tx_msg = volca_sample_2_get_msg (0x4f, buff, buff_size);
   g_free (buff);
