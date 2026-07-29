cf4db207965f9c3ab11977b3e3c5e02fc29a155a
dagargo <dagargo@gmail.com>
2026-07-29 20:29:12 +0200

    volca-sample-2: Add comments to tests
    Downloading an empty sample slot fails so tests require preloading a sample.

--- test/integration/volca_sample_2_sample_fs_tests.sh
+++ test/integration/volca_sample_2_sample_fs_tests.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo "*******************************************"
+echo "* This test requires a sample in slot 199 *"
+echo "*******************************************"
+
 $srcdir/integration/generic_fs_tests.sh --download-ext wav volca-sample-2 sample / 200 /200 /199 ""
 
 exit $?
